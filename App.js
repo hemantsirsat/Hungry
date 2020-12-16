7a619dd9f0929a1a472504be28f4264a95d1820b
Hemant Sirsat <hemantsirsat40@gmail.com>
2020-12-16 21:29:23 +0530

    Bottom Navigation Added

--- App.js
+++ App.js
@@ -1,21 +1,59 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer} from '@react-navigation/native'
+import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
+import HomeScreen from './src/Screen/HomeScreen';
+import Trending from './src/Screen/Trending';
+import About from './src/Screen/About';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+import { Ionicons, Feather } from '@expo/vector-icons';
 
-export default function App() {
+const Tab = createMaterialBottomTabNavigator();
+
+function MyTabs() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Tab.Navigator 
+        activeColor="#000"
+        shifting={true}  
+      >
+        <Tab.Screen 
+          name="Home" 
+          component={HomeScreen} 
+          tabBarColor="ffd500"
+          options={{
+            tabBarLabel: 'Home',
+            tabBarColor:"#bee9e8",
+            tabBarIcon: ({ color }) => (
+              <MaterialCommunityIcons name="home" color={color} size={26} />
+            )
+          }}
+        />
+        <Tab.Screen 
+          name="Settings" 
+          component={Trending} 
+          options={{
+            tabBarLabel: 'Search',
+            tabBarColor:"#62b6cb",
+            tabBarIcon: ({ color }) => (
+              <Ionicons name="search" size={24} color={color} />
+            )
+          }} 
+        />
+        <Tab.Screen 
+          name="About" 
+          component={About} 
+          options={{
+            tabBarLabel: 'About',
+            tabBarColor:"#9eb3c2",
+            tabBarIcon: ({ color }) => (
+              <Feather name="info" size={24} color={color} />
+            )
+          }} 
+        />
+      </Tab.Navigator>
+    </NavigationContainer>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default MyTabs;
---
+++ src/Screen/Trending.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+const Trending =() =>{
+    return(
+        <View>
+            <Text>Trending Screen</Text>
+        </View>
+    );
+};
+
+const styles = StyleSheet.create({});
+
+export default Trending;
