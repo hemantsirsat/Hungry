8485b97ab4ca46ee8688149925cc379822a590c8
Hemant Sirsat <hemantsirsat40@gmail.com>
2020-12-19 23:43:03 +0530

    Navigation with params done

--- src/Screen/RecipeDetails.js
+++ src/Screen/RecipeDetails.js
@@ -1,9 +1,23 @@
 import React from 'react';
-import { Text } from 'react-native';
+import { StatusBar } from 'expo-status-bar';
+import { Text, View, Image, StyleSheet } from 'react-native';
 
 const RecipeDetails = ({ route }) => {
-    console.log(route);
-    return( <Text>Recipe Details.</Text> )
+    const imageURL = route.params.item.recipe.image;
+    return(
+        <View style={{flex:1}}>
+            <StatusBar style="auto" />
+            <Image source={{uri:imageURL}} style={styles.headerimageStyle}/>
+            <Text>{route.params.item.recipe.label}</Text>
+        </View>
+    );
 };
+
+const styles = StyleSheet.create({
+    headerimageStyle:{
+        width:'auto',
+        height:300
+    }
+});
 
 export default RecipeDetails;
